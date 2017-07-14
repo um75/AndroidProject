cfe13ca37e0600be03a561c813d540f3dc592811
um75 <um752003@gmail.com>
2017-07-14 21:04:56 +0900

    로딩화면 디자인 구현 켈린더뷰 라이브러리 추가 및 UI 셋팅(진행중..)

--- app/src/main/java/ap/test/com/androidproject/MainActivity.java
+++ app/src/main/java/ap/test/com/androidproject/MainActivity.java
@@ -1,13 +1,35 @@
 package ap.test.com.androidproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private Thread splashTread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            splashTread = new Thread(){
+                @Override
+                public void run(){
+                    try{
+                        synchronized(this){
+                            wait(5000);
+
+                        }
+                    }catch(InterruptedException e){
+                        Log.v("메인","에러났다 : " + e.toString());
+                    }
+                    finish();
+                    Intent intent = new Intent();
+                    intent.setClass(MainActivity.this, StartActivity.class);
+                    startActivity(intent);
+            }
+        };
+        splashTread.start();
     }
 }
---
+++ app/src/main/java/ap/test/com/androidproject/StartActivity.java
@@ -0,0 +1,21 @@
+package ap.test.com.androidproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+/**
+ * Created by USER on 2017-07-13.
+ */
+
+public class StartActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_start);
+
+    }
+
+
+}
